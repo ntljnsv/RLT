2c025a8ea2d94479b0aac2fda205a1e1d4fe6857
marijanamarijana <marijana.petrovska@students.finki.ukim.mk>
2026-05-18 22:32:12 +0200

    final try

--- typer_training_app/shared/model_loader.py
+++ typer_training_app/shared/model_loader.py
@@ -3,6 +3,7 @@ from transformers import AutoModelForCausalLM, AutoTokenizer, BitsAndBytesConfig
 
 
 def load_model_and_tokenizer(model_id: str, load_in_4bit: bool):
+
     tokenizer = AutoTokenizer.from_pretrained(model_id)
 
     if tokenizer.pad_token is None:
@@ -12,7 +13,6 @@ def load_model_and_tokenizer(model_id: str, load_in_4bit: bool):
 
     if load_in_4bit:
         bnb_config = BitsAndBytesConfig(
-            load_in_4bit=True,
             bnb_4bit_quant_type="nf4",
             bnb_4bit_compute_dtype=torch.bfloat16,
             bnb_4bit_use_double_quant=True,
@@ -20,9 +20,13 @@ def load_model_and_tokenizer(model_id: str, load_in_4bit: bool):
 
     model = AutoModelForCausalLM.from_pretrained(
         model_id,
-        quantization_config=bnb_config,
-        torch_dtype=torch.bfloat16,
+
         device_map="auto",
+
+        torch_dtype=torch.bfloat16,
+
+        quantization_config=bnb_config,
+
         attn_implementation="eager",
     )
 
